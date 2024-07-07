5aaef7539f4b51c97bbd3586f361bf8413c6c481
Seniru <senirupasan@gmail.com>
2024-07-07 12:45:50 +0530

    test: Test input operator (,)

---
+++ test/input.bf.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+(echo -n a; echo -n b; echo -n c;) | ./bf --file ./test/input.bf
--- test/test.sh
+++ test/test.sh
@@ -27,6 +27,7 @@ test "./bf --file ./test/left-right.bf" $'\005\003\005'
 test "./bf --file ./test/loop.bf" $'\005\001\001\002\003\004\005'
 test "./bf --file ./test/nested-loops.bf" $'\024' # 20 base 10 = 24 base 8
 test "./bf --file ./test/simple-if.bf" "10"
+test "./test/input.bf.sh" $'\nProgram input: adf'
 
 echo
 echo "Test #2"
